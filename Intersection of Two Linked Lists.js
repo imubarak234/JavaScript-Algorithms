d4b61d05d31d329dbc74394ba01a610cd7d88fec
Mubarak <imubarak2424@gmail.com>
2022-07-07 10:28:25 +0100

    isrepeated

--- Intersection of Two Linked Lists.js
+++ Intersection of Two Linked Lists.js
@@ -16,8 +16,14 @@ class LinkedList {
     if(this.tail === null)
     this.head = this.tail = newnode;
     else {
-      this.tail.next_node = newnode;
+      this.tail.next = newnode;
       this.tail = newnode;
+    }
+  }
+
+  addArray(arr){
+    for(let x = 0; x < arr.length; x++){
+      this.add(arr[x])
     }
   }
 
@@ -27,8 +33,55 @@ class LinkedList {
     while(node){
       if(index == count)
       return node.value;
-      node = node.next_node;
+      node = node.next;
       count += 1;
     }
   }
+
+  print(){
+    let node = this.head
+    
+    while(node){
+      console.log(node.value)
+      node = node.next
+    }
+  }
+}
+
+const links = new LinkedList()
+const links2 = new LinkedList()
+
+links.addArray([4,1,8,4,5])
+links2.addArray([5,6,1,8,4,5])
+
+console.log(links.head)
+
+const getInersectionNode = () => {
+  let node = links.head
+  let node2 = links2.head
+
+  let countNode = 0;
+  let countNode2 = 0;
+  let array1 = [];
+  let isRepeated = false;
+
+  while(node){
+      countNode++;
+      if(array1.includes(node.value)) isRepeated = true;
+      array1.push(node.value);
+      node = node.next;
+    }
+
+    while(node2){
+      countNode2++;
+      if(array1.includes(node2.value)) isRepeated = true;
+      array1.push(node2.value);
+      node2 = node2.next;
+    }
+
+    node = links.head
+    node2 = links2.head
+
+    
+
 }
