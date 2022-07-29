1c6425f3d527da5f323f0f30277a4b4893df51d4
Mubarak <imubarak2424@gmail.com>
2022-07-29 17:08:31 +0100

    asiah

--- tests.js
+++ tests.js
@@ -42,8 +42,38 @@ else {
 }
 
 
-testscores = 38;
+testscores = 88;
 
 if( (testscores <= 100) && (testscores >= 70) ){
-  
+  console.log("A");
 }
+else if ((testscores < 70) && (testscores >= 60)){
+  console.log("B");
+}
+else if((testscores < 60) && (testscores >= 55)){
+  console.log("C");
+}
+else if((testscores < 55) && (testscores >= 49)){
+  console.log("D");
+}
+else {
+  console.log("F");
+}
+
+if ((testscores > 60) || (testscores < 80)){
+  console.log("You did great")
+}
+
+const arr = [];
+let count = 0;
+
+do {
+  arr.push(count);
+  count++
+} while (count == 5);
+
+if("3" == 3){
+  console.log("Its the same")
+}
+
+console.log(arr);
