e286b0f0d79137751b7e46266db13219abef2ad1
Mubarak <imubarak2424@gmail.com>
2022-07-29 15:19:40 +0100

    lesseon with aisha

--- tests.js
+++ tests.js
@@ -1,104 +1,49 @@
-let myString = "Hello world";
 
-let meInt = 23;
+lessons = null;
 
-let myFloat = 23.47;
+myobject = {};
 
-let myBool = true;
+console.log(typeof(myobject));
 
-let myArray = [true, false, "string", 88]
 
-let myObject = {
-  "one" : 34,
-  "two" : 94,
-  "three" : [43, 89],
-  "four" : "string",
-  five : {
-    six : [2,3,4],
-    seven : [4,5,6],
-  }
-}
+let myvariable = "friday";
 
-console.log("The data type = " + typeof(myArray));
-console.log(myObject['five'].six)
+let isWorking = false;
 
-var myVariable = 90;
-let first = 67;
+console.log(myvariable); 
 
-console.log(myVariable)
+myvariable = "monday"
 
-myVariable = 50
+console.log(myvariable)
 
-console.log(myVariable)
-
-const constant = "string"
-
-console.log(constant)
-
-let second = 'myStirng'
-
-let third = 'string'
-
-if('3' > '3'){
-  console.log("finished")
-}
-else if (typeof(third) == 'string') {
-  console.log('almost there')
+if('5' == '4'){
+  console.log("finish");
+  var condition = "string"
 }
 else {
-  console.log('Not Finished')
+  console.log("no finished");
 }
 
-let testScore = 50;
-
-if((testScore <= 100) && (testScore >= 70)){
-  console.log("A");
-}
-else if((testScore < 70) && (testScore >= 60)){
-  console.log("B");
-}
-else if((testScore < 60) && (testScore >= 50)){
-  console.log('C')
-}
-else if((testScore < 50) && (testScore >= 40)){
-  console.log('D');
+if(9 <= 8) {
+  console.log("success")
 }
 else {
-  console.log('F');
+  console.log("fail")
 }
 
-if((testScore == 50) && (testScore == 70)){
-  console.log("You passed")
+if ( "computer" == "computer" ){
+  console.log("mac is the best");
+}
+else if (89 > 5){
+  console.log("Lunch time");
+}
+else {
+  console.log("unfinished");
 }
 
 
-//Loops 
+testscores = 38;
 
-const arr = [];
-
-
-for (let count = 20; count > 10; count-= 2){
-  arr.push(count)
+if( (testscores <= 100) && (testscores >= 70) ){
+  
 }
-
-//Loops 
-
-//if statements
-
-//conditionals
-
-
-console.log(arr)
-
-function firstFunction(){
-  return "My first function";
-}
-
-function addToScore(args){
-  testScore = testScore + args
-}
-
-addToScore(10)
-addToScore(10)
-console.log(testScore)
-
