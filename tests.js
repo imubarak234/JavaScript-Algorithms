c0a48deb596cbe56c32cdebe68c009f6e70d81bd
Mubarak <imubarak2424@gmail.com>
2022-08-03 16:54:53 +0100

    new work

--- tests.js
+++ tests.js
@@ -64,16 +64,73 @@ if ((testscores > 60) || (testscores < 80)){
   console.log("You did great")
 }
 
-const arr = [];
+const box = [];
 let count = 0;
 
 do {
-  arr.push(count);
-  count++
-} while (count == 5);
+  box.push(count)
+  count++;
+}while (count == 10)
 
-if("3" == 3){
-  console.log("Its the same")
+console.log(box)
+
+
+const box2 = [];
+let count2 = 0;
+
+while(count2 < 20){
+  count2++;
+  box2.push(count2);
+  
 }
 
-console.log(arr);
+console.log(box2)
+
+const box3 = [];
+let count3;
+
+for(count3 = 0; count3 < 20; count3++){
+  box3.push(count3);
+}
+
+console.log(box3)
+
+function print(morning){
+  let box = [];
+  for(let x = 0; x < morning; x++){
+    box.push(x);
+  }
+  console.log(box)
+}
+
+print(9);
+
+function print(morning){
+  let box = [];
+  for(let x = 0; x < morning; x++){
+    box.push(x);
+  }
+  console.log(box)
+}
+
+print(9);
+
+function scores(testscores){
+  if( (testscores <= 100) && (testscores >= 70) ){
+    console.log("A");
+  }
+  else if ((testscores < 70) && (testscores >= 60)){
+    console.log("B");
+  }
+  else if((testscores < 60) && (testscores >= 55)){
+    console.log("C");
+  }
+  else if((testscores < 55) && (testscores >= 49)){
+    console.log("D");
+  }
+  else {
+    console.log("F");
+  }
+}
+
+scores(50)
